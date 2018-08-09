735e0e4608c71265c49ff678fbc0f74a57503768
Sarat Annapaneni <sannapaneni@health.nyc.gov>
2018-08-09 16:51:00 -0400

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DocSearchComponent } from './doc-search/doc-search.component';
 import { DocUploadComponent } from './doc-upload/doc-upload.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: DocSearchComponent },
+  { path: 'upload', component: DocUploadComponent }
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +19,8 @@ import { DocUploadComponent } from './doc-upload/doc-upload.component';
     DocUploadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
