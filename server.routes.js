3ca4d32320ebc7ab17d5f6e70a7dd9fa287c0e95
Sarat Annapaneni <sannapaneni@health.nyc.gov>
2018-08-10 16:59:09 -0400

    asd

--- server.routes.js
+++ server.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Busboy = require('busboy');
 const inspect = require('util').inspect;
+const azure = require('azure-storage');
+const blobService = azure.createBlobService(process.env.storageConnectionString);
 
 /* GET api listing. */
 router.get('/', (req, res) => {
@@ -13,6 +15,7 @@ router.post('/upload', (req, res) => {
   var busboy = new Busboy({ headers: req.headers });
 
   busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+    /*
     console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
     file.on('data', function (data) {
       console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
@@ -20,6 +23,33 @@ router.post('/upload', (req, res) => {
     file.on('end', function () {
       console.log('File [' + fieldname + '] Finished');
     });
+    */
+
+    function readFirstBytes() {
+      var chunk = file.read(5);
+      if (!chunk)
+        return file.once('readable', readFirstBytes);
+      var type = fileType(chunk);
+      if (type.ext === 'jpg' || type.ext === 'png' || type.ext === 'gif') {
+        const blobStream = blobSvc.createWriteStreamToBlockBlob(
+          storageName,
+          blobName,
+          function (error) {
+            if (error)
+              console.log('blob upload error', error);
+            else
+              console.log('blob upload complete');
+          }
+        );
+        file.unshift(chunk);
+        file.pipe(blobStream);
+      } else {
+        console.error('Rejected file of type ' + type);
+        file.resume(); // Drain file stream to continue processing form
+      }
+    }
+
+    readFirstBytes();
   });
 
   busboy.on('field', function (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
