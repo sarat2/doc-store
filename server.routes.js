07c10660e3c8f729a919737661775cd4f8d4729d
Sarat Annapaneni <sarat2@outlook.com>
2018-08-11 15:49:49 -0400

    upload

--- server.routes.js
+++ server.routes.js
@@ -1,7 +1,9 @@
+var http = require("https");
 const express = require('express');
 const router = express.Router();
 const Busboy = require('busboy');
 const inspect = require('util').inspect;
+const uuidv1 = require('uuid/v1');
 const azure = require('azure-storage');
 const blobService = azure.createBlobService(process.env.storageConnectionString);
 
@@ -31,6 +33,7 @@ router.post('/upload', (req, res) => {
         return file.once('readable', readFirstBytes);
       // var type = fileType(chunk);
       // if (type.ext === 'jpg' || type.ext === 'png' || type.ext === 'gif') {
+        console.log(filename);
         const blobStream = blobService.createWriteStreamToBlockBlob("docs", filename,
           function (error, response) {
             if (error)
@@ -54,10 +57,59 @@ router.post('/upload', (req, res) => {
   });
 
   busboy.on('field', function (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-    console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    // console.log(JSON.parse(val));
+    // console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+
+    // console.log(uuidv1());
+    var doc = JSON.parse(val);
+    doc['@search.action'] = 'upload';
+    doc['id'] = uuidv1();
+    
+    var options = {
+      "method": "POST",
+      "hostname": "docustore.search.windows.net",
+      "path": "/indexes/docs/docs/index?api-version=2017-11-11",
+      "headers": {
+        "content-type": "application/json",
+        "api-key": process.env.searchKey,
+        "cache-control": "no-cache"
+      }
+    };
+    
+    var request = http.request(options, function (response) {
+      var chunks = [];
+    
+      response.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+    
+      response.on("end", function () {
+        var body = Buffer.concat(chunks);
+        console.log(body.toString());
+      });
+    });
+
+    request.write(JSON.stringify({ value: [ doc ] }));
+    request.end();
+    // req.write(JSON.stringify({ value: 
+    //    [ { '@search.action': 'upload',
+    //        id: '3dc43e10-9d91-11e8-9787-c398b547b7b2',
+    //        appName: 'Contracts',
+    //        appKey: '123456',
+    //        contractNo: '19AX04567R0A01',
+    //        fmsRegNo: '2019000001',
+    //        startDate: '07/01/2018',
+    //        endDate: '06/30/2019',
+    //        documentName: 'ExecutedContractCopy.pdf',
+    //        category: 'Supporting',
+    //        subCategory: 'Executed',
+    //        tags: [] } ] }));
+    // req.end();
+
   });
 
   busboy.on('finish', function () {
+    console.log('finish');
     res.writeHead(200, { 'Connection': 'close' });
     res.end();
   });
@@ -66,3 +118,39 @@ router.post('/upload', (req, res) => {
 });
 
 module.exports = router;
+
+
+
+/*
+var request = require("request");
+
+var options = { method: 'POST',
+  url: 'https://docustore.search.windows.net/indexes/docs/docs/index',
+  qs: { 'api-version': '2017-11-11' },
+  headers: 
+   { 'postman-token': '37ed7622-cc01-7f5e-b8fb-55c5deed1c8d',
+     'cache-control': 'no-cache',
+     'api-key': 'CB4664734B38E6542A1306216AC8D929',
+     'content-type': 'application/json' },
+  body: 
+   { value: 
+      [ { '@search.action': 'upload',
+          id: '3dc43e10-9d91-11e8-9787-c398b547b7b2',
+          appName: 'Contracts',
+          appKey: '123456',
+          contractNo: '19AX04567R0A01',
+          fmsRegNo: '2019000001',
+          startDate: '07/01/2018',
+          endDate: '06/30/2019',
+          documentName: 'ExecutedContractCopy.pdf',
+          category: 'Supporting',
+          subCategory: 'Executed',
+          tags: [] } ] },
+  json: true };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+*/
--- src/app/doc-upload/doc-upload.component.ts
+++ src/app/doc-upload/doc-upload.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HttpClient, HttpEventType } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-doc-upload',
@@ -11,6 +11,8 @@ export class DocUploadComponent implements OnInit {
   metadata: FormGroup = null;
   appName: string = null;
   selectedFile: File = null;
+  success = false;
+  progress: string = null;
 
   constructor(private http: HttpClient) { }
 
@@ -24,31 +26,51 @@ export class DocUploadComponent implements OnInit {
   onAppChange(event) {
     this.appName = event.target.value;
     this.metadata = new FormGroup({
-      contractNo: new FormControl('Contract1', [Validators.required]),
+      appName: new FormControl(event.target.value, [Validators.required]),
+      appKey: new FormControl('123456', [Validators.required]),
+      contractNo: new FormControl('19AX04567R0A01', [Validators.required]),
       fmsRegNo: new FormControl('2019000001', [Validators.required]),
       startDate: new FormControl(this.formatShortDate(new Date('07/01/2018')), [Validators.required]),
       endDate: new FormControl(this.formatShortDate(new Date('06/30/2019')), [Validators.required]),
-      document: new FormControl('', [Validators.required])
+      category: new FormControl('', [Validators.required]),
+      subCategory: new FormControl('', []),
+      documentName: new FormControl('', [Validators.required])
     });
   }
 
   onFileSelected(event) {
     this.selectedFile = <File>event.target.files[0];
-    this.metadata.controls['document'].setValue(this.selectedFile.name);
+    this.metadata.controls['documentName'].setValue(this.selectedFile.name);
+    this.success = false;
+    this.progress = null;
+  }
+
+  formatFileName() {
+    let fn = this.metadata.value.category + '\\';
+    if (this.metadata.value.subCategory !== '') {
+      fn += this.metadata.value.subCategory + '\\';
+    }
+    return fn + this.metadata.value.documentName;
   }
 
   onSubmit() {
+    console.log(this.formatFileName());
     const fd = new FormData();
-    fd.append('file', this.selectedFile, this.selectedFile.name);
-    // fd.append('fields', this.metadata.value);
-    this.http.post('/api/upload', fd, {
-      reportProgress: true,
-      observe: 'events'
-    }).subscribe(event => {
+    fd.append('file', this.selectedFile, this.formatFileName());
+    fd.append('metadata', JSON.stringify(this.metadata.value));
+
+    // no need to set headers explicitly
+    // const headers = new HttpHeaders();
+    // headers.set('Accept', 'application/json');
+    // headers.set('Content-Disposition', 'form-data');
+
+    this.http.post('/api/upload', fd, { reportProgress: true, observe: 'events' }).subscribe(event => {
       if (event.type === HttpEventType.UploadProgress) {
-        console.log(Math.round(event.loaded / event.total * 100));
+        const p = Math.round(event.loaded / event.total * 100);
+        this.progress = p + '%' + ((p === 100) ? ' - COMPLETED' : '');
       } else if (event.type === HttpEventType.Response) {
         console.log(event);
+        this.success = (event.status === 200);
       }
     });
   }
